cd9dcf5080ceb91aeb3065214a293046ef8951ef
Daniel Barowy <dbarowy@cs.umass.edu>
2015-11-06 15:56:42 -0500

    skeleton

---
+++ ExceLint/Program.fs
@@ -0,0 +1,20 @@
+﻿namespace ExceLint
+    open COMWrapper
+
+    module Analysis =
+
+        [<EntryPoint>]
+        let main argv = 
+            let filename = argv.[0]
+
+            let app = new COMWrapper.Application()
+
+            let workbook = app.OpenWorkbook(filename)
+
+            let dag = workbook.buildDependenceGraph()
+
+            printfn "%s" (dag.ToDOT())
+
+            let baz = System.Console.ReadLine()
+
+            0
