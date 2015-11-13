907c71be6e017832896d76a12a9c00496d0554ba
Daniel Barowy <dbarowy@cs.umass.edu>
2015-11-13 16:18:20 -0500

    compute probabilities

--- ExceLint/Program.fs
+++ ExceLint/Program.fs
@@ -6,16 +6,17 @@
 
         [<EntryPoint>]
         let main argv = 
-            let filename = @"..\..\..\ExceLintTests\TestData\Public_debt-ratios_advanced-SummarySheetOnly.xlsx"
-            let output = "output.csv"
+            let INPUTFILE = @"..\..\..\ExceLintTests\TestData\Public_debt-ratios_advanced-SummarySheetOnly.xlsx"
+            let OUTPUTFILE = "output.csv"
+            let ALPHA = 0.05
 
-            let outFile = new StreamWriter(output)
+            let outFile = new StreamWriter(OUTPUTFILE)
 
             printfn "Starting Excel..."
             let app = new COMWrapper.Application()
 
             printfn "Opening workbook..."
-            let workbook = app.OpenWorkbook(filename)
+            let workbook = app.OpenWorkbook(INPUTFILE)
 
             printfn "Building dependence graph..."
             let dag = workbook.buildDependenceGraph()
@@ -24,15 +25,49 @@
             let allCells = dag.allCells()
 
             let cellDegrees = Array.map (fun cell -> cell, (Degree.getIndegreeForCell cell dag), (Degree.getOutdegreeForCell cell dag)) allCells
+            let cellDegreesGTZero = Array.filter (fun (_, indeg, outdeg) -> (indeg + outdeg) > 0) cellDegrees
+            let totalCells = System.Convert.ToDouble(cellDegreesGTZero.Length)
+
+            printfn "Computing probabilities..."
+            // histogram for indegree
+            let hist_indeg = Array.fold (fun (m: Map<int,int>) (_, indeg, _) ->
+                                if (m.ContainsKey(indeg)) then
+                                    m.Add(indeg, 1)
+                                else
+                                    m.Add(indeg, m.Item(indeg) + 1)
+                             ) (new Map<int,int>([])) cellDegreesGTZero
+            // histogram for outdegree
+            let hist_outdeg = Array.fold (fun (m: Map<int,int>) (_, _, outdeg) ->
+                                if (m.ContainsKey(outdeg)) then
+                                    m.Add(outdeg, 1)
+                                else
+                                    m.Add(outdeg, m.Item(outdeg) + 1)
+                              ) (new Map<int,int>([])) cellDegreesGTZero
+
+            // histogram for combined
+            let hist_combo = Array.fold (fun (m: Map<int,int>) (_, indeg, outdeg) ->
+                                let combined = indeg + outdeg
+                                if (m.ContainsKey(combined)) then
+                                    m.Add(combined, 1)
+                                else
+                                    m.Add(combined, m.Item(combined) + 1)
+                             ) (new Map<int,int>([])) cellDegreesGTZero
 
             printfn "Writing output..."
-            Array.sortBy (fun (_, indeg, outdeg) -> indeg + outdeg) cellDegrees |>
-            Array.map (fun (addr: AST.Address, indeg: int, outdeg: int) ->
-                let a1_addr = addr.A1Local()
-                let row = sprintf "\"%s\",%i,%i" a1_addr indeg outdeg
-                outFile.WriteLine(row)
-            )
-            |> ignore
+            let headers = [| "\"cell\",\"indegree\",\"outdegree\",\"combined\",\"pr_indeg\",\"pr_outdeg\",\"pr_combined\",\"indeg_anom\",\"outdeg_anom\",\"combo_anom\"" |]
+            let output = Array.sortBy (fun (_, indeg, outdeg) -> - (indeg + outdeg)) cellDegrees |>
+                         Array.map (fun (addr: AST.Address, indeg: int, outdeg: int) ->
+                            let a1_addr = addr.A1Local()
+                            let combined = indeg + outdeg
+                            let indeg_prob = System.Convert.ToDouble(hist_indeg.Item(indeg)) / totalCells
+                            let outdeg_prob = System.Convert.ToDouble(hist_outdeg.Item(outdeg)) / totalCells
+                            let combo_prob = System.Convert.ToDouble(hist_combo.Item(combined)) / totalCells
+                            let indeg_anom = indeg_prob < ALPHA
+                            let outdeg_anom = outdeg_prob < ALPHA
+                            let combo_anom = combo_prob < ALPHA
+                            sprintf "\"%s\",%i,%i,%i,%f,%f,%f,%b,%b,%b" a1_addr indeg outdeg combined indeg_prob outdeg_prob combo_prob indeg_anom outdeg_anom combo_anom
+                         )
+            Array.map (fun (row: string) -> outFile.WriteLine(row)) (Array.append headers output) |> ignore
 
             outFile.Close()
 
