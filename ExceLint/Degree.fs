b17bae88c1bc158ccbabc3eb1be6a30ad822dd98
Daniel Barowy <dbarowy@cs.umass.edu>
2015-11-13 14:39:17 -0500

    Degree analysis.

---
+++ ExceLint/Degree.fs
@@ -0,0 +1,38 @@
+﻿module Degree
+    open COMWrapper
+    open Depends
+
+    /// <summary>Gets the number of inputs referenced by fAddr.</summary>
+    /// <param name="fAddr">the address of a formula cell</param>
+    /// <param name="dag">a DAG</param>
+    /// <returns>the number of inputs referenced by fAddr</returns>
+    let getIndegreeForFormulaCell(fAddr: AST.Address)(dag: DAG) : int =
+        let allInputAddresses = (dag.getFormulaSingleCellInputs fAddr |> List.ofSeq)
+                                @ (dag.getFormulaInputVectors fAddr
+                                    |> List.ofSeq
+                                    |> List.map (fun rng -> rng.Addresses() |> List.ofArray)
+                                    |> List.concat)
+        allInputAddresses.Length
+
+    /// <summary>Gets the number of inputs referenced by fAddr.</summary>
+    /// <param name="cell">the address of an arbitrary cell</param>
+    /// <param name="dag">a DAG</param>
+    /// <returns>the number of inputs referenced by cell</returns>
+    let getIndegreeForCell(cell: AST.Address)(dag: DAG) =
+        if dag.isFormula(cell) then
+            getIndegreeForFormulaCell cell dag
+        else
+            0
+
+    /// <summary>Gets the number of formulas that reference a cell.</summary>
+    /// <param name="cell">the address of an arbitrary cell</param>
+    /// <param name="dag">a DAG</param>
+    /// <returns>the number of formulas that reference a cell</returns>
+    let getOutdegreeForCell(cell: AST.Address)(dag: DAG) =
+        let referencingFormulas = dag.getFormulasThatRefCell cell
+        let formulasThatRefRanges =
+            Array.map (fun rng ->
+                dag.getFormulasThatRefVector(rng)
+            ) (dag.getVectorsThatRefCell cell) |> Array.concat
+
+        referencingFormulas.Length + formulasThatRefRanges.Length
--- ExceLint/Program.fs
+++ ExceLint/Program.fs
@@ -1,18 +1,37 @@
 ﻿namespace ExceLint
     open COMWrapper
+    open System.IO
 
     module Analysis =
 
         [<EntryPoint>]
         let main argv = 
-            let filename = @"..\..\..\ExceLintTests\TestData\7_gradebook_xls.xlsx"
+            let filename = @"..\..\..\ExceLintTests\TestData\Public_debt-ratios_advanced-SummarySheetOnly.xlsx"
+            let output = "output.csv"
 
+            let outFile = new StreamWriter(output)
+
+            printfn "Starting Excel..."
             let app = new COMWrapper.Application()
 
+            printfn "Opening workbook..."
             let workbook = app.OpenWorkbook(filename)
 
+            printfn "Building dependence graph..."
             let dag = workbook.buildDependenceGraph()
 
-            printfn "%s" (dag.ToDOT())
+            printfn "Computing indegree and outdegree..."
+            let allCells = dag.allCells()
 
+            let cellDegrees = Array.map (fun cell -> cell, (Degree.getIndegreeForCell cell dag), (Degree.getOutdegreeForCell cell dag)) allCells
+
+            printfn "Writing output..."
+            Array.map (fun (addr: AST.Address, indeg: int, outdeg: int) ->
+                let row = sprintf "%s,%i,%i" (addr.ToString()) indeg outdeg
+                outFile.WriteLine(row)
+            ) cellDegrees |> ignore
+
+            outFile.Close()
+
+            printfn "Done."
             0
