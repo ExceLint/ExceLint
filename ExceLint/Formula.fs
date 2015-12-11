b92f6ba6bb1898f6c67bae15ccc16d997d147ca5
Emma Tosch <etosch@cs.umass.edu>
2015-12-11 03:58:04 -0500

    formula features

---
+++ ExceLint/Formula.fs
@@ -0,0 +1,20 @@
+﻿module Formula
+
+    open Feature
+    open Depends
+    open Parcel
+
+    type MaxNestingDepth() = 
+        inherit BaseFeature()
+
+        static member private getMaxNestingDepth (fAddr: AST.Address) (dag: DAG) =
+            let rec findMaxPath (formulaAST: AST.Expression) (depth: int) =
+                match formulaAST with
+                | ReferenceExpr _ -> depth 
+                | BinOpExpr _ e1 e2 -> 1 + max (findMaxPath e1 depth) (findMaxPath e2 depth)
+                | UnaryOpExpr _ e -> 1 + findMaxPath e
+                | ParensExpr e -> 1 + findMaxPath e
+            in findMaxPath (parseFormulaAtAddress fAddr dag) 0
+           
+        static member run cell dag =
+            MaxNestingDepth.getMaxNestingDepth cell dag
