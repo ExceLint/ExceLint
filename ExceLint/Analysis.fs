de15468b3d143470b32a3f2b7817621c2753c3e8
Daniel W. Barowy <dbarowy@cs.umass.edu>
2016-03-29 17:54:51 -0400

    move things around so that it is easy to get run times.

--- ExceLint/Analysis.fs
+++ ExceLint/Analysis.fs
@@ -3,13 +3,16 @@
     open System.Collections
     open System
     open ConfUtils
+    open PerfUtils.TimedLambda
+
+        type ScoreTable = Dict<string,(AST.Address*double)[]>
+        type FreqTable = Dict<string*Scope.SelectID*double,int>
+        type Ranking = KeyValuePair<AST.Address,double>[]
 
         // a C#-friendly error model constructor
         type ErrorModel(config: FeatureConf, dag: Depends.DAG, alpha: double, progress: Depends.Progress) =
-
-            // train model on construction
-            // a score table: featurename -> (address, score)
-            let _data: Dict<string,(AST.Address*double)[]> =
+                
+            let runEnabledFeatures() =
                 config.EnabledFeatures |>
                 Array.map (fun fname ->
                     // get feature lambda
@@ -23,9 +26,8 @@
                     
                     fname, fvals
                 ) |> adict
-
-            // a frequency table: (featurename, selector, score) -> freq
-            let ftable: Dict<string*Scope.SelectID*double,int> =
+            
+            let buildFrequencyTable(data: ScoreTable): FreqTable =
                 let d = new Dict<string*Scope.SelectID*double,int>()
                 Array.iter (fun fname ->
                     Array.iter (fun (sel: Scope.Selector) ->
@@ -37,7 +39,7 @@
                             else
                                 d.Add((fname,sID,score), 1)
                             progress.IncrementCounter()
-                        ) (_data.[fname])
+                        ) (data.[fname])
                     ) (Scope.Selector.Kinds)
                 ) (config.EnabledFeatures)
                 d
@@ -45,18 +47,18 @@
             /// <summary>Analyzes the given cell using all of the configured features and produces a score.</summary>
             /// <param name="cell">the address of a formula cell</param>
             /// <returns>a score</returns>
-            member self.score(cell: AST.Address)(fname: string) : double =
+            let score(cell: AST.Address)(fname: string) : double =
                 // get feature by name
                 let f = config.FeatureByName fname
 
                 // get feature value for this cell
                 f cell dag
 
-            member private self.rankByFeatureAndSelector(fname: string)(dag: Depends.DAG)(sel: Scope.Selector) : (AST.Address*int)[] =
+            let rankByFeatureAndSelector(fname: string)(dag: Depends.DAG)(sel: Scope.Selector)(ftable: FreqTable) : (AST.Address*int)[] =
                 // sort by least common
                 let sorted = Array.map (fun addr ->
                                  let sID = sel.id addr
-                                 let score = self.score addr fname
+                                 let score = score addr fname
                                  let freq = ftable.[(fname,sID,score)]
                                  addr, freq
                              ) (dag.allCells()) |>
@@ -64,12 +66,12 @@
                 // return the address and its rank
                 sorted |> Array.mapi (fun i (addr,freq) -> addr,i )
 
-            member private self.rankAllFeatures(dag: Depends.DAG)(sel: Scope.Selector) : Dict<AST.Address,int[]> =
+            let rankAllFeatures(dag: Depends.DAG)(sel: Scope.Selector)(ftable: FreqTable) : Dict<AST.Address,int[]> =
                 // get per-feature rank for every cell
                 let d = new Dict<AST.Address,int[]>()
 
                 Array.iteri (fun i fname ->
-                    let ranks = self.rankByFeatureAndSelector fname dag sel
+                    let ranks = rankByFeatureAndSelector fname dag sel ftable
                     Array.iter (fun (addr,rank) ->
                         if d.ContainsKey addr then
                             let arr = d.[addr]
@@ -83,7 +85,7 @@
                 ) (config.EnabledFeatures)
                 d
 
-            member private self.sumFeatureRanksAndSort(ranks: Dict<AST.Address,int[]>) : KeyValuePair<AST.Address,double>[] =
+            let sumFeatureRanksAndSort(ranks: Dict<AST.Address,int[]>) : Ranking =
                 Seq.map (fun (kvp: KeyValuePair<AST.Address,int[]>) ->
                     new KeyValuePair<AST.Address,double>(kvp.Key, double(Array.sum (kvp.Value)))
                 ) ranks
@@ -92,7 +94,7 @@
 
             // sum the count of the appropriate feature bin of every feature
             // for the given address
-            member private self.sumFeatureCounts(addr: AST.Address)(sel: Scope.Selector) : int =
+            let sumFeatureCounts(addr: AST.Address)(sel: Scope.Selector)(ftable: FreqTable) : int =
                 Array.sumBy (fun fname -> 
                     // get feature
                     let feature = config.FeatureByName fname
@@ -104,45 +106,22 @@
                     ftable.[(fname,sID,fscore)]
                 ) (config.EnabledFeatures)
 
-            member self.rankByFeatureSum() : KeyValuePair<AST.Address,double>[] =
-                // get sums for every address
-                // and for every enabled scope
-                let addrSums: (AST.Address*int)[] =
-                    Array.map (fun addr ->
-                        let sum = Array.sumBy (fun sel ->
-                                        self.sumFeatureCounts addr Scope.Selector.AllCells
-                                  ) (config.EnabledScopes)
-                        addr, sum
-                    ) (dag.allCells())
-
-                // rank by sum (smallest first)
-                let rankedAddrs: (AST.Address*int)[] = Array.sortBy (fun (addr,sum) -> sum) addrSums
-
-                // return KeyValuePairs
-                let totalOrder = Array.map (fun (addr,sum) -> new KeyValuePair<AST.Address,double>(addr,double sum)) rankedAddrs
-
-                let winners = self.cutRankBySignificance totalOrder
-
-                winners
-
-            member private self.mergeRanks(multiRanks: Dict<AST.Address,int[]>[])(dag: Depends.DAG) : Dict<AST.Address,int[]> =
-                let d = new Dict<AST.Address, int[]>()
-
-                Array.iter (fun addr ->
-                    let allRanks = 
-                        Array.fold (fun (acc: int list)(ranking: Dict<AST.Address,int[]>) ->
-                            (ranking.[addr] |> List.ofArray) @ acc
-                        ) ([]) multiRanks
-                    d.Add(addr, allRanks |> List.toArray)
-                ) (dag.allCells())
-
-                d
+            // "AngleMin" algorithm
+            let dderiv(y: int[]) : int =
+                let mutable anglemin = 1
+                let mutable angleminindex = 0
+                for index in 0..(y.Length - 3) do
+                    let angle = y.[index] + y.[index + 2] - 2 * y.[index + 1]
+                    if angle < anglemin then
+                        anglemin <- angle
+                        angleminindex <- index
+                angleminindex
 
             /// <summary>
             /// Finds the maxmimum significance height
             /// </summary>
             /// <returns>maxmimum significance height count (an int)</returns>
-            member self.getSignificanceCutoff : int =
+            let _significanceCutoff : int =
                 // round to integer
                 int (
                     // get total number of counts
@@ -151,8 +130,8 @@
                     * alpha
                 )
 
-            member private self.cutRankBySignificance(ranking: KeyValuePair<AST.Address,double>[]): KeyValuePair<AST.Address,double>[] =
-                let cutoff = self.getSignificanceCutoff
+            let cutRankBySignificance(ranking: Ranking): Ranking =
+                let cutoff = _significanceCutoff
 
                 let cutrank = Array.fold (fun (acc: KeyValuePair<AST.Address,double> list)(score: KeyValuePair<AST.Address,double>) ->
                                     if score.Value > double cutoff then
@@ -163,24 +142,49 @@
 
                 let rank_nums = Array.map (fun (kvp: KeyValuePair<AST.Address,double>) -> int(kvp.Value)) cutrank
 
-                let dderiv_idx = self.dderiv(rank_nums)
+                let dderiv_idx = dderiv(rank_nums)
 
                 cutrank.[0..dderiv_idx]
 
-            /// <summary>Ranks all the cells in the workbook by their anomalousness.</summary>
-            /// <returns>an KeyValuePair<AST.Address,int>[] of (address,score) ranked from most to least anomalous</returns>
-            member self.rankWithScore() : KeyValuePair<AST.Address,double>[] =
-                // get the number of features
-                let fsize = double(config.EnabledFeatures.Length)
-
-                // find per-feature ranks for every cell in the DAG and compute total rank
-                let theRankings = Array.map (fun scope ->
-                                      self.rankAllFeatures dag scope
+            let rank(ftable: FreqTable) : Ranking =
+                // get sums for every address
+                // and for every enabled scope
+                let addrSums: (AST.Address*int)[] =
+                    Array.map (fun addr ->
+                        let sum = Array.sumBy (fun sel ->
+                                        sumFeatureCounts addr Scope.Selector.AllCells ftable
                                   ) (config.EnabledScopes)
+                        addr, sum
+                    ) (dag.allCells())
 
-                let mergedRankings = self.mergeRanks theRankings dag
+                // rank by sum (smallest first)
+                let rankedAddrs: (AST.Address*int)[] = Array.sortBy (fun (addr,sum) -> sum) addrSums
 
-                self.sumFeatureRanksAndSort mergedRankings
+                // return KeyValuePairs
+                let totalOrder = Array.map (fun (addr,sum) -> new KeyValuePair<AST.Address,double>(addr,double sum)) rankedAddrs
+
+                let winners = cutRankBySignificance totalOrder
+
+                winners
+
+            // get scores for each feature: featurename -> (address, score)[]
+            let _data,_score_time = runMillis runEnabledFeatures ()
+
+            // build frequency table: (featurename, selector, score) -> freq
+            let _ftable,_ftable_time = runMillis buildFrequencyTable _data
+
+            // rank
+            let _ranking,_ranking_time = runMillis rank _ftable
+
+            member self.ScoreTimeInMilliseconds : int64 = _score_time
+
+            member self.FrequencyTableTimeInMilliseconds : int64 = _ftable_time
+
+            member self.RankingTimeInMilliseconds : int64 = _ranking_time
+
+            member self.rankByFeatureSum() : Ranking = _ranking
+
+            member self.getSignificanceCutoff : int = _significanceCutoff
 
             member self.inspectSelectorFor(addr: AST.Address, sel: Scope.Selector) : KeyValuePair<AST.Address,(string*double)[]>[] =
                 let sID = sel.id addr
@@ -216,12 +220,3 @@
 
                 debug |> Seq.toArray
 
-            member self.dderiv(y: int[]) : int =
-                let mutable anglemin = 1
-                let mutable angleminindex = 0
-                for index in 0..(y.Length - 3) do
-                    let angle = y.[index] + y.[index + 2] - 2 * y.[index + 1]
-                    if angle < anglemin then
-                        anglemin <- angle
-                        angleminindex <- index
-                angleminindex
--- PerfUtils/PerfUtils.fs
+++ PerfUtils/PerfUtils.fs
@@ -2,16 +2,24 @@
 
 open System
 
-type TimedLambda<'A,'B>(f: 'A -> 'B, input: 'A) = 
-    let _sw = new Diagnostics.Stopwatch()
-    let mutable _o : 'B option = None
-    do
+module TimedLambda =
+    type TimeUnit =
+    | Milliseconds
+    | Ticks
+
+    let private run<'A,'B>(f: 'A -> 'B)(input: 'A)(tu: TimeUnit) : 'B*int64 =
+        let _sw = new Diagnostics.Stopwatch()
         _sw.Start()
-        _o <- Some (f input)
+        let _o = f input
         _sw.Stop()
-    member self.Output =
-        match _o with
-        | Some(o) -> o
-        | None -> failwith "Something went wrong!"
-    member self.ElapsedMilliseconds = _sw.ElapsedMilliseconds
-    member self.ElapsedTicks = _sw.ElapsedTicks
+        let tu_amount =
+            match tu with
+            | Milliseconds -> _sw.ElapsedMilliseconds
+            | Ticks -> _sw.ElapsedTicks
+        (_o,tu_amount)
+
+    let runMillis<'A,'B>(f: 'A -> 'B)(input: 'A) : 'B*int64 =
+        run f input Milliseconds
+
+    let runTicks<'A,'B>(f: 'A -> 'B)(input: 'A) : 'B*int64 =
+        run f input Ticks
