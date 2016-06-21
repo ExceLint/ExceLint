543f8bfe3a1498ad58957936c74cafc4dab7c1c7
Daniel Barowy <dbarowy@cs.umass.edu>
2016-06-20 22:26:44 -0400

    debug-friendliness

--- ExceLint/Vector.fs
+++ ExceLint/Vector.fs
@@ -201,7 +201,8 @@
                     Array.map (fun v -> relativeToTail v dag isOSI)
                 else
                     Array.map (fun v -> relativeToOrigin v dag isOSI)
-            rebase (vectors cell dag depth isMixed)
+            let output = rebase (vectors cell dag depth isMixed)
+            output
 
         type DeepInputVectorRelativeL2NormSum() = 
             inherit BaseFeature()
