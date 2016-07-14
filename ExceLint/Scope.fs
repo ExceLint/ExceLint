fa6426d6a9cd42faeed23f975976297b018ed323
Daniel W. Barowy <dbarowy@cs.umass.edu>
2016-07-14 14:23:33 -0400

    Include 'SameLevel' in scope enumeration

--- ExceLint/Scope.fs
+++ ExceLint/Scope.fs
@@ -124,4 +124,4 @@
             | LevelID(levels) ->
                 let lstrs = Set.map (fun l -> l.ToString()) levels |> Set.toList
                 "Levels [" + (List.reduce (fun (acc: string)(l: string) -> acc + ", " + l.ToString()) lstrs) + "]"
-        static member Kinds = [| AllCells; SameColumn; SameRow |]
+        static member Kinds = [| AllCells; SameColumn; SameRow; SameLevel |]
