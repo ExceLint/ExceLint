6f5642068981aa553e23d0ba7ba91e55397b5798
Daniel W. Barowy <dbarowy@cs.umass.edu>
2016-07-29 11:17:51 -0400

    Add F# Data for CSV type provider and add custom logger type.

---
+++ ExceLintRunner/CSV.fs
@@ -0,0 +1,9 @@
+﻿module CSV
+
+open FSharp.Data
+
+type ExceLintRunnerLogger = 
+  CsvProvider<Schema = "benchmark_name (string), num_cells (int), num_formulas (int),\
+    sig_thresh (float), dep_time_ms (int64), score_time_ms (int64), freq_time_ms (int64), ranking_time_ms (int64), num_anom (int),\
+    opt_cond_all_cells (bool), opt_cond_rows (bool), opt_cond_cols (bool), opt_cond_levels (bool),\
+    opt_addrmode_inference (bool), opt_weight_intrinsic_anom (bool), opt_weight_condition_set_sz (bool)", HasHeaders=false>
