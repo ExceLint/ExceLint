97f7e62b347bd128ad5514fb6bdc51cdc36ac97e
Daniel W. Barowy <dbarowy@cs.umass.edu>
2016-08-26 17:36:28 -0400

    simpler options for cli

--- ExceLintRunner/Args.fs
+++ ExceLintRunner/Args.fs
@@ -16,57 +16,62 @@ open System.Text.RegularExpressions
         member self.DebugPath = Path.Combine(opath, "debug.csv")
 
     let usage() : unit =
-        printfn "ExceLintRunner.exe [input directory] [output path] [java path] [opt_verbose] [opt1 ... opt7]"
+        printfn "ExceLintRunner.exe <input directory> <output path> <java path> [flags]"
         printfn 
-            "Recursively finds all Excel (*.xls and *.xlsx) files in [input directory], \n\
+            "Recursively finds all Excel (*.xls and *.xlsx) files in <input directory>, \n\
             opens them, runs ExceLint, and prints output statistics to a file called \n\
-            exceline_output.csv in [output path].\n\n\
-            [java path] and [CUSTODES path] are needed in order to conduct a comparison\n\
+            exceline_output.csv in <output path>.\n\n\
+            <java path> and <CUSTODES path> are needed in order to conduct a comparison\n\
             against the CUSTODES tool.\n\n\
             Press Ctrl-C to cancel an analysis."
         printfn "\nwhere\n"
-        printfn "opt_verbose = <true> or <false>: log per-spreadsheet flagged cells as separate csvs"
-        printfn "opt1 = <true> or <false>: condition by all cells"
-        printfn "opt2 = <true> or <false>: condition by columns"
-        printfn "opt3 = <true> or <false>: condition by rows"
-        printfn "opt4 = <true> or <false>: condition by levels"
-        printfn "opt5 = <true> or <false>: infer address modes"
-        printfn "opt6 = <true> or <false>: weigh by intrinsic anomalousness"
-        printfn "opt7 = <true> or <false>: weigh by conditioning set size"
-        printfn "\nExample:"
+        printfn "[flags] consists of any of the following options, which are TRUE when"
+        printfn "present and FALSE when omitted:"
+        printfn "\n"
+        printfn "-verbose    log per-spreadsheet flagged cells as separate csvs"
+        printfn "-spectral   use spectral outliers, otherwise use summation outliers;"
+        printfn "            forces the use of -sheets below and disables -allcells,"
+        printfn "            -columns, -rows, and -levels"
+        printfn "-allcells   condition by all cells"
+        printfn "-columns    condition by columns"
+        printfn "-rows       condition by rows"
+        printfn "-levels     condition by levels"
+        printfn "-sheets     condition by sheets"
+        printfn "-addrmode   infer address modes"
+        printfn "-intrinsic  weigh by intrinsic anomalousness"
+        printfn "-css        weigh by conditioning set size"
+        printfn "\nExample:\n"
         printfn "ExceLintRunner \"C:\\data\" output.csv true true true true true false false true"
+        printfn "ExceLintRunner.exe \"C:\\data\" \"C:\\output\" \"C:\\ProgramData\\Oracle\\Java\\javapath\\java.exe\" \"C:\\CUSTODES2\\cc2.jar\" -verbose -allcells -rows -columns -levels -css"
 
         System.Environment.Exit(1)
 
     let processArgs(argv: string[]) : Config =
-        if argv.Length <> 12 then
+        if argv.Length < 4 || argv.Length > 14 then
             usage()
         let dpath = argv.[0]    // input directory
         let opath = argv.[1]    // output directory
         let jpath = argv.[2]    // java path
         let cpath = argv.[3]    // CUSTODES path
 
-        let optVerbose = System.Boolean.Parse(argv.[4])
-        let optCondAllCells = System.Boolean.Parse(argv.[5])
-        let optCondCols = System.Boolean.Parse(argv.[6])
-        let optCondRows = System.Boolean.Parse(argv.[7])
-        let optCondLevels = System.Boolean.Parse(argv.[8])
-        let optAddrMode = System.Boolean.Parse(argv.[9])
-        let optIntrinsicAnom = System.Boolean.Parse(argv.[10])
-        let optCondSetSz = System.Boolean.Parse(argv.[11])
-
         let csv = Path.Combine(opath, "excelint_output.csv")
 
-        let mutable featureConf = new ExceLint.FeatureConf()
+        let flags = argv.[4 .. argv.Length - 1]
 
-        featureConf <- featureConf.enableShallowInputVectorMixedL2NormSum()
-        featureConf <- if optCondAllCells then featureConf.analyzeRelativeToAllCells() else featureConf
-        featureConf <- if optCondCols then featureConf.analyzeRelativeToColumns() else featureConf
-        featureConf <- if optCondRows then featureConf.analyzeRelativeToRows() else featureConf
-        featureConf <- if optCondLevels then featureConf.analyzeRelativeToLevels() else featureConf
-        featureConf <- if optAddrMode then featureConf.inferAddressModes() else featureConf
-        featureConf <- if optIntrinsicAnom then featureConf.weightByIntrinsicAnomalousness() else featureConf
-        featureConf <- if optCondSetSz then featureConf.weightByConditioningSetSize() else featureConf
+        let (isVerb,fConf) = Array.fold (fun (isVerb: bool,conf: ExceLint.FeatureConf) flag ->
+                                 match flag with
+                                 | "-verbose" -> true, conf
+                                 | "-spectral" -> isVerb, conf.spectralRanking()
+                                 | "-allcells" -> isVerb, if conf.IsEnabledSpectralRanking then conf else conf.analyzeRelativeToAllCells()
+                                 | "-columns" -> isVerb, if conf.IsEnabledSpectralRanking then conf else conf.analyzeRelativeToColumns()
+                                 | "-rows" -> isVerb, if conf.IsEnabledSpectralRanking then conf else conf.analyzeRelativeToRows()
+                                 | "-levels" -> isVerb, if conf.IsEnabledSpectralRanking then conf else conf.analyzeRelativeToLevels()
+                                 | "-sheets" -> isVerb, if conf.IsEnabledSpectralRanking then conf.analyzeRelativeToSheet() else conf
+                                 | "-addrmode" -> isVerb, conf.inferAddressModes()
+                                 | "-intrinsic" -> isVerb, conf.weightByIntrinsicAnomalousness()
+                                 | "-css" -> isVerb, conf.weightByConditioningSetSize()
+                                 | s -> failwith ("Unrecognized option: " + s)
+                             ) (false,new ExceLint.FeatureConf()) flags
 
-        Config(dpath, opath, jpath, cpath, optVerbose, csv, featureConf)
+        Config(dpath, opath, jpath, cpath, isVerb, csv, fConf)
 
