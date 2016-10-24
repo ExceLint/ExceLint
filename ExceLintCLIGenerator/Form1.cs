fc84307606d51ddcd81f5c824eb8b5c5416794d1
Daniel W. Barowy <dbarowy@cs.umass.edu>
2016-10-24 19:12:14 -0400

    good enough for now

--- ExceLintCLIGenerator/Form1.cs
+++ ExceLintCLIGenerator/Form1.cs
@@ -1,11 +1,31 @@
 ﻿using System;
 using System.Windows.Forms;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Diagnostics;
+using System.Threading;
 
 namespace ExceLintCLIGenerator
 {
+
+
     public partial class Form1 : Form
     {
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        private static extern int GetShortPathName(
+            [MarshalAs(UnmanagedType.LPTStr)] string path,
+            [MarshalAs(UnmanagedType.LPTStr)] StringBuilder shortPath,
+            int shortPathLength
+         );
+
+        private static string shortPath(string p)
+        {
+            var sb = new StringBuilder(255);
+            GetShortPathName(p, sb, sb.Capacity);
+            return sb.ToString();
+        }
+
         public Form1()
         {
             InitializeComponent();
@@ -63,101 +83,121 @@ namespace ExceLintCLIGenerator
             Properties.Settings.Default["rowsFlagDefault"] = rowsCheckBox.Checked;
             Properties.Settings.Default["levelsFlagDefault"] = levelsCheckBox.Checked;
             Properties.Settings.Default["sheetsFlagDefault"] = sheetsCheckbox.Checked;
+            Properties.Settings.Default.Save();
         }
 
         private void excelintrunnerPathTextBox_TextChanged(object sender, EventArgs e)
         {
             Properties.Settings.Default["excelintrunnerPathDefault"] = excelintrunnerPathTextBox.Text;
+            Properties.Settings.Default.Save();
         }
 
         private void benchmarkDirTextbox_TextChanged(object sender, EventArgs e)
         {
             Properties.Settings.Default["inputPathDefault"] = benchmarkDirTextbox.Text;
+            Properties.Settings.Default.Save();
         }
 
         private void outputDirectoryTextbox_TextChanged(object sender, EventArgs e)
         {
             Properties.Settings.Default["outputPathDefault"] = outputDirectoryTextbox.Text;
+            Properties.Settings.Default.Save();
         }
 
         private void excelintGroundTruthCSVTextbox_TextChanged(object sender, EventArgs e)
         {
             Properties.Settings.Default["excelintGTPathDefault"] = excelintGroundTruthCSVTextbox.Text;
+            Properties.Settings.Default.Save();
         }
 
         private void custodesGroundTruthCSVTextbox_TextChanged(object sender, EventArgs e)
         {
             Properties.Settings.Default["custodesGTPathDefault"] = custodesGroundTruthCSVTextbox.Text;
+            Properties.Settings.Default.Save();
         }
 
         private void javaPathTextbox_TextChanged(object sender, EventArgs e)
         {
             Properties.Settings.Default["javaPathDefault"] = javaPathTextbox.Text;
+            Properties.Settings.Default.Save();
         }
 
         private void thresholdTextBox_TextChanged(object sender, EventArgs e)
         {
             Properties.Settings.Default["thresholdDefault"] = Double.Parse(thresholdTextBox.Text);
+            Properties.Settings.Default.Save();
         }
 
         private void custodesJARPathTextbox_TextChanged(object sender, EventArgs e)
         {
             Properties.Settings.Default["custodesJARPathDefault"] = custodesJARPathTextbox.Text;
+            Properties.Settings.Default.Save();
         }
 
         private void allcellsCheckBox_CheckedChanged(object sender, EventArgs e)
         {
             Properties.Settings.Default["allCellsFlagDefault"] = allcellsCheckBox.Checked;
+            Properties.Settings.Default.Save();
         }
 
         private void columnsCheckBox_CheckedChanged(object sender, EventArgs e)
         {
             Properties.Settings.Default["columnsFlagDefault"] = columnsCheckBox.Checked;
+            Properties.Settings.Default.Save();
         }
 
         private void rowsCheckBox_CheckedChanged(object sender, EventArgs e)
         {
             Properties.Settings.Default["rowsFlagDefault"] = rowsCheckBox.Checked;
+            Properties.Settings.Default.Save();
         }
 
         private void levelsCheckBox_CheckedChanged(object sender, EventArgs e)
         {
             Properties.Settings.Default["levelsFlagDefault"] = levelsCheckBox.Checked;
+            Properties.Settings.Default.Save();
         }
 
         private void sheetsCheckbox_CheckedChanged(object sender, EventArgs e)
         {
             Properties.Settings.Default["sheetsFlagDefault"] = sheetsCheckbox.Checked;
+            Properties.Settings.Default.Save();
         }
 
         private void addrmodeCheckBox_CheckedChanged(object sender, EventArgs e)
         {
             Properties.Settings.Default["addrmodeFlagDefalt"] = addrmodeCheckBox.Checked;
+            Properties.Settings.Default.Save();
         }
 
         private void weighIntrinsicCheckBox_CheckedChanged(object sender, EventArgs e)
         {
             Properties.Settings.Default["intrinsicFlagDefault"] = weighIntrinsicCheckBox.Checked;
+            Properties.Settings.Default.Save();
         }
 
         private void weighCSSCheckBox_CheckedChanged(object sender, EventArgs e)
         {
             Properties.Settings.Default["cssFlagDefault"] = weighCSSCheckBox.Checked;
+            Properties.Settings.Default.Save();
         }
 
         private void analyzeInputsCheckBox_CheckedChanged(object sender, EventArgs e)
         {
             Properties.Settings.Default["inputsTooFlagDefault"] = analyzeInputsCheckBox.Checked;
+            Properties.Settings.Default.Save();
         }
 
         private void verboseCheckBox_CheckedChanged(object sender, EventArgs e)
         {
             Properties.Settings.Default["verboseFlagDefault"] = verboseCheckBox.Checked;
+            Properties.Settings.Default.Save();
         }
 
         private void noexitCheckBox_CheckedChanged(object sender, EventArgs e)
         {
             Properties.Settings.Default["noexitFlagDefault"] = noexitCheckBox.Checked;
+            Properties.Settings.Default.Save();
         }
 
         private string[] generateCLIInvocation(bool includeEXE)
@@ -183,14 +223,14 @@ namespace ExceLintCLIGenerator
 
             var flags = new List<string>();
 
-            if (includeEXE) flags.Add('"' + excelintrunnerPathTextBox.Text + '"');
-            flags.Add('"' + benchmarkDirTextbox.Text + '"');
-            flags.Add('"' + outputDirectoryTextbox.Text + '"');
-            flags.Add('"' + excelintGroundTruthCSVTextbox.Text + '"');
-            flags.Add('"' + custodesGroundTruthCSVTextbox.Text + '"');
-            flags.Add('"' + javaPathTextbox.Text + '"');
-            flags.Add('"' + custodesJARPathTextbox.Text + '"');
-            flags.Add(thresholdTextBox.Text);
+            if (includeEXE) flags.Add(shortPath(excelintrunnerPathTextBox.Text));
+            flags.Add(shortPath(benchmarkDirTextbox.Text));
+            flags.Add(shortPath(outputDirectoryTextbox.Text));
+            flags.Add(shortPath(excelintGroundTruthCSVTextbox.Text));
+            flags.Add(shortPath(custodesGroundTruthCSVTextbox.Text));
+            flags.Add(shortPath(javaPathTextbox.Text));
+            flags.Add(shortPath(custodesJARPathTextbox.Text));
+            flags.Add("-thresh " + thresholdTextBox.Text);
 
             if (verboseCheckBox.Checked) flags.Add("-verbose");
             if (noexitCheckBox.Checked) flags.Add("-noexit");
@@ -218,11 +258,35 @@ namespace ExceLintCLIGenerator
             Clipboard.SetText(generateCLIInvocationString());
         }
 
+        public static void runCommand(string cpath, string[] args)
+        {
+            using (var p = new Process())
+            {
+                // notice that we're using the Windows shell here and the unix-y 2>&1
+                p.StartInfo.FileName = @"c:\windows\system32\cmd.exe";
+                p.StartInfo.Arguments = "/c \"" + cpath + " " + String.Join(" ", args) + "\" 2>&1";
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.RedirectStandardOutput = false;
+                p.StartInfo.RedirectStandardError = false;
+
+                using (var outputWaitHandle = new AutoResetEvent(false))
+                {
+                    // start process
+                    p.Start();
+
+                    // wait for process to terminate
+                    p.WaitForExit();
+
+                    // wait on handle
+                    outputWaitHandle.WaitOne();
+                }
+            }
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
             var flags = generateCLIInvocation(includeEXE: false);
-            var o = new Output(excelintrunnerPathTextBox.Text, flags);
-            o.Show();
+            runCommand(shortPath(excelintrunnerPathTextBox.Text), flags);
         }
     }
 }
