460a360d0e66702dbc8a0aabbe8ad393bc66fb37
Daniel Barowy <dbarowy@cs.umass.edu>
2016-11-28 11:43:11 -0500

    Add ToString for resultant vector for easier debugging.

--- ExceLint/Feature.fs
+++ ExceLint/Feature.fs
@@ -9,6 +9,11 @@ module Feature
     type Countable =
     | Num of double
     | Vector of int*int*int
+        override self.ToString() : string =
+            match self with
+            | Num d -> d.ToString()
+            | Vector(x,y,z) ->
+                "<" + x.ToString() + "," + y.ToString() + "," + z.ToString() + ">"
 
     type Capability = { enabled : bool; kind: ConfigKind; runner: AST.Address -> Depends.DAG -> Countable; }
 
