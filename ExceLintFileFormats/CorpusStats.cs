8c7dd3a17f6b064ed9ba8d3c397640419fe74c75
Daniel W. Barowy <dbarowy@cs.umass.edu>
2016-12-01 17:32:19 -0500

    new file format

---
+++ ExceLintFileFormats/CorpusStats.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using CsvHelper;
+
+namespace ExceLintFileFormats
+{
+    public class CorpusStats : IDisposable
+    {
+        StreamWriter _sw;
+        CsvWriter _cw;
+
+        public CorpusStats(string path)
+        {
+            _sw = new StreamWriter(path);
+            _cw = new CsvWriter(_sw);
+
+            // write header
+            _cw.WriteHeader<DebugInfoRow>();
+
+            _sw.Flush();
+        }
+
+        public void WriteRow(CorpusStatsRow row)
+        {
+            _cw.WriteRecord(row);
+        }
+
+        #region IDisposable Support
+        // To detect redundant calls
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _sw.Flush();
+                    _cw.Dispose();
+
+                    _cw = null;
+                    _sw = null;
+                }
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
+    }
+
+    public class CorpusStatsRow
+    {
+        public string Workbook { get; set; }
+        public string Operator { get; set; }
+        public int Count { get; set; }
+    }
+}
