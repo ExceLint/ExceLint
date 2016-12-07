c6145e45f8164b2c31dfc0c416896a7fd03c16f5
Daniel W. Barowy <dbarowy@cs.umass.edu>
2016-12-06 19:41:15 -0500

    Add exception log.

---
+++ ExceLintFileFormats/ExceptionLog.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using CsvHelper;
+
+namespace ExceLintFileFormats
+{
+    public class ExceptionLog : IDisposable
+    {
+        StreamWriter _sw;
+        CsvWriter _cw;
+
+        public ExceptionLog(string path)
+        {
+            _sw = new StreamWriter(path);
+            _sw.AutoFlush = true;
+            _cw = new CsvWriter(_sw);
+
+            // write header
+            _cw.WriteHeader<ExceptionLogRow>();
+        }
+
+        public void WriteRow(ExceptionLogRow row)
+        {
+            _cw.WriteRecord(row);
+        }
+
+        #region IDisposable Support
+        // To detect redundant calls
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _sw.Flush();
+                    _cw.Dispose();
+
+                    _cw = null;
+                    _sw = null;
+                }
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
+    }
+
+    public class ExceptionLogRow
+    {
+        public string Workbook { get; set; }
+        public string Error { get; set; }
+    }
+}
