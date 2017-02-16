a9edf6f60c646d2694b9710eaada8a129713df03
Daniel W. Barowy <dbarowy@cs.umass.edu>
2017-02-16 16:55:30 -0500

    grr

--- ExceLint/Feature.fs
+++ ExceLint/Feature.fs
@@ -7,6 +7,14 @@ module Feature
     | Scope
     | Misc
 
+//    type ICountable<'T>=
+//        abstract member Add: 'T -> 'T
+//
+//    type Num(d: double) =
+//        member self.d = d
+//        interface ICountable<Num> with
+//            member self.Add(n: Num) = Num(d + n.d)
+
     type Countable =
     | Num of double
     | Vector of double*double*double
@@ -19,22 +27,17 @@ module Feature
                 "<" + x.ToString() + "," + y.ToString() + "," + z.ToString() + ">"
             | SquareVector(dx,dy,dz,x,y,z) ->
                 "<" + dx.ToString() + "," + dy.ToString() + "," + dz.ToString() + "," + x.ToString() + "," + y.ToString() + "," + z.ToString() +  ">"
-            | ResultantVectorWithConstant(x,y,z,c) ->
-                "<" + x.ToString() + "," + y.ToString() + "," + z.ToString() + "," + c.ToString() + ">"
         member self.MeanFoldDefault : Countable =
             match self with
             | Num _ -> Num(0.0)
             | Vector _ -> Vector(0.0,0.0,0.0)
             | SquareVector _ -> SquareVector(0.0,0.0,0.0,0.0,0.0,0.0)
-            | ResultantVectorWithConstant _ -> ResultantVectorWithConstant(0.0,0.0,0.0,0.0)
         member self.Add(co: Countable) : Countable =
             match (self,co) with
             | Num d1, Num d2 -> Num(d1 + d2)
             | Vector(x1,y1,z1), Vector(x2,y2,z2) -> Vector(x1 + x2, y1 + y2, z1 + z2)
             | SquareVector(dx1,dy1,dz1,x1,y1,z1), SquareVector(dx2,dy2,dz2,x2,y2,z2) ->
                 SquareVector(dx1 + dx2, dy1 + dy2, dz1 + dz2, x1 + x2, y1 + y2, z1 + z2)
-            | ResultantVectorWithConstant(x1,y1,z1,c1),
-                ResultantVectorWithConstant(x2,y2,z2,c2) -> ResultantVectorWithConstant(x1+x2, y1+y2, z1+z2, c1+c2)
             | _ -> failwith "Invalid operation.  Both Countables must be of the same type."
         member self.Negate : Countable =
             match self with
@@ -42,24 +45,18 @@ module Feature
             | Vector(x,y,z) -> Vector(-x, -y, -z)
             | SquareVector(dx,dy,dz,x,y,z) ->
                 SquareVector(-dx,-dy,-dz,-x,-y,-z)
-            | ResultantVectorWithConstant(x,y,z,c) -> ResultantVectorWithConstant(-x,-y,-z,-c)
         member self.ScalarDivide(d: double) : Countable =
             match self with
             | Num n -> Num(n / d)
             | Vector(x,y,z) -> Vector(x / d, y / d, z / d)
             | SquareVector(dx,dy,dz,x,y,z) ->
                 SquareVector(dx / d, dy / d, dz / d, x / d, y / d, z / d)
-            | ResultantVectorWithConstant(x,y,z,c) ->
-                ResultantVectorWithConstant(x / d, y / d, z / d, c / d)
         member self.VectorMultiply(co: Countable) : double =
             match (self,co) with
             | Num d1, Num d2 -> d1 * d2
             | Vector(x1,y1,z1), Vector(x2,y2,z2) -> x1 * x2 + y1 * y2 + z1 * z2
             | SquareVector(dx1,dy1,dz1,x1,y1,z1), SquareVector(dx2,dy2,dz2,x2,y2,z2) ->
                 dx1 * dx2 + dy1 * dy2 + dz1 * dz2 + x1 * x2 + y1 * y2 + z1 * z2
-            | ResultantVectorWithConstant(x1,y1,z1,c1),
-                ResultantVectorWithConstant(x2,y2,z2,c2) ->
-                x1 * x2 + y1 * y2 + z1 * z2 + c1 * c2
             | _ -> failwith "Invalid operation.  Both Countables must be of the same type."
         member self.Sqrt : Countable =
             match self with
@@ -67,16 +64,12 @@ module Feature
             | Vector(x,y,z) -> Vector(Math.Sqrt(x), Math.Sqrt(y), Math.Sqrt(z))
             | SquareVector(dx,dy,dz,x,y,z) ->
                 SquareVector(Math.Sqrt(dx), Math.Sqrt(dy), Math.Sqrt(dz), Math.Sqrt(x), Math.Sqrt(y), Math.Sqrt(z))
-            | ResultantVectorWithConstant(x,y,z,c) ->
-                ResultantVectorWithConstant(Math.Sqrt(x),Math.Sqrt(y),Math.Sqrt(z),Math.Sqrt(c))
         member self.Abs : Countable =
             match self with
             | Num n -> Num(Math.Abs(n))
             | Vector(x,y,z) -> Vector(Math.Abs(x),Math.Abs(y),Math.Abs(z))
             | SquareVector(dx,dy,dz,x,y,z) ->
                 SquareVector(Math.Abs(dx),Math.Abs(dy),Math.Abs(dz),Math.Abs(x),Math.Abs(y),Math.Abs(z))
-            | ResultantVectorWithConstant(x,y,z,c) ->
-                ResultantVectorWithConstant(Math.Abs(x),Math.Abs(y),Math.Abs(z),Math.Abs(c))
 
     type Capability = { enabled : bool; kind: ConfigKind; runner: AST.Address -> Depends.DAG -> Countable; }
 
