330625b642c7d7eafd38a0754af9ec6d06d3527f
Daniel Barowy <dbarowy@cs.umass.edu>
2017-05-23 16:55:31 -0400

    save partially-coded clustering sanity check code

---
+++ ExceLint/Distances.fs
@@ -0,0 +1,68 @@
+﻿namespace ExceLint
+    open System
+    open System.Collections.Generic
+    open Utils
+    open CommonTypes
+    open CommonFunctions
+
+    module Distances =
+        // define distance
+        let min_dist(hb_inv: InvertedHistogram) =
+            (fun (source: HashSet<AST.Address>)(target: HashSet<AST.Address>) ->
+                let pairs = cartesianProduct source target
+
+                let compute = (fun (a,b) ->
+                                let (_,_,ac) = hb_inv.[a]
+                                let (_,_,bc) = hb_inv.[b]
+                                ac.EuclideanDistance bc
+                                )
+                let f = (fun (a,b) ->
+                            compute (a,b)
+                        )
+                let minpair = argmin f pairs
+                let dist = f minpair
+                dist * (double source.Count)
+            )
+
+        // this is kinda-sorta EMD; it has no notion of flows because I have
+        // no idea what that means in terms of spreadsheet formula fixes
+        let earth_movers_dist(hb_inv: InvertedHistogram) =
+            (fun (source: HashSet<AST.Address>)(target: HashSet<AST.Address>) ->
+                let compute = (fun (a,b) ->
+                                let (_,_,ac) = hb_inv.[a]
+                                let (_,_,bc) = hb_inv.[b]
+                                ac.EuclideanDistance bc
+                                )
+
+                let f = (fun (a,b) ->
+                            compute (a,b)
+                        )
+
+                // for every point in source, find the closest point in target
+                let ds = source
+                            |> Seq.map (fun addr ->
+                                let pairs = target |> Seq.map (fun t -> addr,t)
+                                let min_dist: double = pairs |> Seq.map f |> Seq.min
+                                min_dist
+                            )
+
+                Seq.sum ds
+            )
+
+        // define distance (min distance between clusters)
+        let cent_dist(hb_inv: InvertedHistogram) =
+            (fun (source: HashSet<AST.Address>)(target: HashSet<AST.Address>) ->
+                // Euclidean distance with a small twist:
+                // The distance between any two cells on different
+                // sheets is defined as infinity.
+                // This ensures that we always agglomerate intra-sheet
+                // before agglomerating inter-sheet.
+                let s_centroid = centroid source hb_inv
+                let t_centroid = centroid target hb_inv
+                let dist = if s_centroid.SameSheet t_centroid then
+                                s_centroid.EuclideanDistance t_centroid
+                            else
+                                Double.PositiveInfinity
+                dist * (double source.Count)
+            )
+
---
+++ ExceLint/KMedioidsClusterModelBuilder.fs
@@ -0,0 +1,111 @@
+﻿namespace ExceLint
+    open System
+    open System.Collections.Generic
+    open Utils
+    open CommonTypes
+    open CommonFunctions
+    open Distances
+
+    type MedoidClustering = Dict<AST.Address,HashSet<AST.Address>>
+
+    module KMedioidsClusterModelBuilder =
+        let private dist(d: DistanceF)(addr1: AST.Address)(addr2: AST.Address) : double =
+            let addr1hs = new HashSet<AST.Address>([addr1])
+            let addr2hs = new HashSet<AST.Address>([addr2])
+            d addr1hs addr2hs
+
+        let private findClosestMedioids(medioids: AST.Address[])(points: AST.Address[])(d: DistanceF) : MedoidClustering =
+            let clustering =
+                points |>
+                Array.fold (fun (acc: MedoidClustering)(point: AST.Address) ->
+                    let closest = medioids |> argmin (fun m -> dist d point m)
+                    if acc.ContainsKey closest then
+                        acc.[closest].Add point |> ignore
+                    else
+                        let hs = new HashSet<AST.Address>()
+                        hs.Add(point) |> ignore
+                        acc.Add(closest, hs) |> ignore
+                    acc
+                ) (new Dict<AST.Address,HashSet<AST.Address>>())
+            clustering
+
+        let private kmedioids(k: int)(cells: AST.Address[])(s: ScoreTable)(hb_inv: InvertedHistogram)(d: DistanceF)(r: Random) : Clustering =
+            // choose k random indices using rejection sampling
+            let seeds = Array.fold (fun xs i ->
+                            let mutable ri = r.Next(cells.Length)
+                            while Set.contains ri xs do
+                                ri <- r.Next(cells.Length)
+                            Set.add ri xs
+                        ) Set.empty [| 0 .. k - 1 |]
+                        |> Set.toArray
+
+            // get the medioid point representations
+            let mutable medioids: AST.Address[] = seeds |> Array.map (fun i -> cells.[i])
+
+            // associate every point with its closest medioid
+            let mutable clustering = findClosestMedioids medioids cells d
+
+            let mutable cost_decreased = true
+
+            // while the cost decreases, find new medoids
+            while cost_decreased do
+                cost_decreased <- false
+
+                // for each medioid
+                for m in medioids do
+                    // swap medioid and any other point
+                    for c in cells do
+                        
+                        failwith "hang on!"
+
+
+            failwith "gettin there"
+
+
+        let runClusterModel(input: Input)(k: int) : AnalysisOutcome =
+            try
+                if (analysisBase input.config input.dag).Length <> 0 then
+                    // initialize selector cache
+                    let selcache = Scope.SelectorCache()
+
+                    // determine the set of cells to be analyzed
+                    let cells = analysisBase input.config input.dag
+
+                    // get all NLFRs for every formula cell
+                    let _runf = fun () -> runEnabledFeatures cells input.dag input.config input.progress
+                    let (ns: ScoreTable,score_time: int64) = PerfUtils.runMillis _runf ()
+
+                    // scale
+                    let nlfrs: ScoreTable = ScaleBySheet ns
+
+                    // make HistoBin lookup by address
+                    let hb_inv = invertedHistogram nlfrs selcache input.dag input.config
+
+                    // DEFINE DISTANCE
+                    let DISTANCE =
+                        match input.config.DistanceMetric with
+                        | DistanceMetric.NearestNeighbor -> min_dist hb_inv
+                        | DistanceMetric.EarthMover -> earth_movers_dist hb_inv
+                        | DistanceMetric.MeanCentroid -> cent_dist hb_inv
+
+                    // init RNG
+                    let r = new Random()
+
+                    // get clustering
+                    let c = kmedioids k cells nlfrs hb_inv DISTANCE r
+
+                    Success(Cluster
+                        {
+                            scores = m.Scores;
+                            ranking = m.Ranking;
+                            score_time = m.ScoreTimeMs;
+                            ranking_time = m.RankingTimeMs;
+                            sig_threshold_idx = 0;
+                            cutoff_idx = m.Cutoff;
+                            weights = new Dictionary<AST.Address,double>();
+                        }
+                    )
+                else
+                    CantRun "Cannot perform analysis. This worksheet contains no formulas."
+            with
+            | AnalysisCancelled -> Cancellation
