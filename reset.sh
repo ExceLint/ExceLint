63f7c5e03c5e467e1de1b0eb1b19a853ce01c1c2
Daniel W. Barowy <dbarowy@cs.williams.edu>
2017-08-30 13:42:41 -0400

    no longer necessary

--- reset.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-git checkout ../data/spreadsheets/custodes/*.xls
-git checkout ../data/spreadsheets/custodes/*.XLS
-git checkout ../analysis/ground_truth_using_visualization/*.xls
-git checkout ../analysis/ground_truth_using_visualization/*.xlsx
-
