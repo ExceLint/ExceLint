376332690a08c023133fae2b73b21359821e243f
Daniel W. Barowy <dbarowy@cs.williams.edu>
2018-03-29 18:34:59 -0400

    Update to F# 4.1

--- ExceLintUI/Properties/AssemblyInfo.cs
+++ ExceLintUI/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Security;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ExceLintUI")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
